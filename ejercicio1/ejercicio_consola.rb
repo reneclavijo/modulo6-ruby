d09fa8b92b0384f8a9f1c10c95af471fabd94cd3
rene rodriguez <reneclavijo@live.com>
2021-08-30 21:55:02 -0500

    agregando el primer commit

---
+++ ejercicio1/ejercicio_consola.rb
@@ -0,0 +1,16 @@
+puts "¡Hola Mundo!"
+puts("¿cómo estas?")
+
+puts "Ingresa tu nombre"
+nombre_usuario = gets.chomp
+
+puts "hola normal " + nombre_usuario
+puts "hola compuesto #{nombre_usuario}"
+
+puts "¿que edad tienes?"
+edad = gets.chomp
+puts "#{edad} eso es mucho tiempo 💀🎂"
+
+puts "¿donde vives?"
+lugar_vivienda = gets.chomp
+puts "¿dónde queda #{lugar_vivienda}? 🧨"
