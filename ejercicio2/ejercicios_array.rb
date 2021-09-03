95eddd604893bf874bb6d07cf5a5475f65c382b7
rene rodriguez <reneclavijo@live.com>
2021-09-02 21:55:24 -0500

    agregando el ejercicio 3 de array

--- ejercicio2/ejercicios_array.rb
+++ ejercicio2/ejercicios_array.rb
@@ -1,36 +1,54 @@
+# n = gets.chomp.to_i
+# # numeros = Array.new
+# numeros = []
+
+# # guardar los números
+# for i in 1..n
+#     numeros << rand(0..10)
+# end
+
+# # sumar los números del arreglo
+# suma = 0
+# puts "primera forma".center(20, "_")
+# for numero in numeros
+#     suma += numero
+#     print "#{numero} "
+# end
+# puts "suma total #{suma}"
+
+# puts "segunda forma".center(20, "_")
+# suma2 = 0
+# for c in 0..numeros.length - 1  
+#     suma2 += numeros[c]
+# end
+# puts "suma total2 #{suma2}"
+
+# puts "tercera forma".center(20, "_")
+# suma3 = 0
+# numeros.each do |numero_con_each|
+#     suma3 += numero_con_each
+# end
+# puts "suma total3 #{suma3}"
+
+# 3. 
+
+puts "Ingrese un número"
 n = gets.chomp.to_i
-# numeros = Array.new
 numeros = []
-
-# guardar los números
 for i in 1..n
-    numeros << rand(0..10)
+    numeros.push rand(0..10)
 end
-
-# sumar los números del arreglo
-suma = 0
-puts "primera forma".center(20, "_")
+textos = []
 for numero in numeros
-    suma += numero
-    print "#{numero} "
+    if numero % 2 == 0
+        textos.push "par"
+    else
+        textos.push "impar"
+    end
 end
-puts "suma total #{suma}"
 
-puts "segunda forma".center(20, "_")
-suma2 = 0
-for c in 0..numeros.length - 1  
-    suma2 += numeros[c]
+for c in 0..numeros.size-1
+    puts numeros[c]
+    puts textos[c]
 end
-puts "suma total2 #{suma2}"
-
-puts "tercera forma".center(20, "_")
-suma3 = 0
-numeros.each do |numero_con_each|
-    suma3 += numero_con_each
-end
-puts "suma total3 #{suma3}"
-
-
-
-
 
