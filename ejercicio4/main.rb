44e116fbbd0533b7d23f1b0a91fb4eb6d1e21846
rene rodriguez <reneclavijo@live.com>
2021-09-09 20:44:26 -0500

    agregando impresión del arreglo de usuario con gustos

--- ejercicio4/main.rb
+++ ejercicio4/main.rb
@@ -5,5 +5,7 @@ include ModuloUsuarios                  # incluye el módulo usuarios
 include ModuloImpresiones               # incluye el módulo impresiones
 
 arreglo_usuarios_correo_faker = generar_usuario_con_correo(5)   # módulo usuarios
-
 imprimir_usuarios_con_correos(arreglo_usuarios_correo_faker)    # módulo impresiones
+
+arreglo_usuario_con_gustos = generar_usuarios_con_gustos(5)
+imprimir_usuarios_con_gustos(arreglo_usuario_con_gustos)
--- ejercicio4/modulo_impresiones.rb
+++ ejercicio4/modulo_impresiones.rb
@@ -5,7 +5,6 @@ module ModuloImpresiones
     end
 
     def imprimir_usuarios_con_correos(arreglo_usuarios)
-        
         espacio = 2
 
         conteo_letras = arreglo_usuarios.map{ |usuario| usuario[:nombre].length }
@@ -14,8 +13,30 @@ module ModuloImpresiones
         conteo_letras_correo = arreglo_usuarios.map{ |usuario| usuario[:email].length }
         align_correo = conteo_letras_correo.max + espacio
 
+        puts "Nombre".ljust(align_nombre) + " | " + "Email".ljust(align_correo) + " | " + "Password"
+        puts
         arreglo_usuarios.each { |usuario|
             puts "#{usuario[:nombre].ljust(align_nombre)  } | #{usuario[:email].ljust(align_correo)} | #{usuario[:password]}"
         }
     end
+
+    def imprimir_usuarios_con_gustos(arreglo_usuarios)
+        espacio = 4
+
+        conteo_letras = arreglo_usuarios.map{ |usuario| usuario[:nombre].length }
+        align_nombre = conteo_letras.max + espacio
+
+        conteo_letras_juego = arreglo_usuarios.map{ |usuario| usuario[:juego].length }
+        align_juego = conteo_letras_juego.max + espacio
+
+        conteo_letras_pelicula = arreglo_usuarios.map{ |usuario| usuario[:pelicula].length }
+        align_pelicula = conteo_letras_pelicula.max + espacio
+
+        puts "Nombre".ljust(align_nombre, "-") + " | " + "Juego".ljust(align_juego, "-") + " | " + "Película".ljust(align_pelicula, "-")
+        puts
+        arreglo_usuarios.each { |usuario|
+            puts "#{usuario[:nombre].ljust(align_nombre, ".")  } | #{usuario[:juego].ljust(align_juego, ".")} | #{usuario[:pelicula].ljust(align_pelicula, ".")}"
+        }
+    end
+
 end
